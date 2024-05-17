236ae864892e53026bc6537955e380b0a27b6b2e
devanshuchaudhary <52855516+devdchaudhary@users.noreply.github.com>
2024-05-17 12:51:17 +0530

    Initial Commit

---
+++ CombinePokemon/CombinePokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinePokemonApp.swift
+//  CombinePokemon
+//
+//  Created by devdchaudhary on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CombinePokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
